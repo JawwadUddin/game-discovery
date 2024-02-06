3b0a5c6def904c1743e243b444fc4c06df9f21a6
Jawwad Uddin <m.m.j.uddin12@gmail.com>
2024-02-06 17:46:49 +0000

    installed Chakra UI + added test button

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import "./App.css";
+import { Button, ButtonGroup } from "@chakra-ui/react";
 
 function App() {
   return (
     <>
-      <h1>Game Discovery</h1>
+      <Button colorScheme="blue">Button</Button>
     </>
   );
 }

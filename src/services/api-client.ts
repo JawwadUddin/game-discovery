adeced57adbcf729adde68f7675a952a78711d59
Jawwad Uddin <m.m.j.uddin12@gmail.com>
2024-02-18 21:04:52 +0000

    Refactoring - fetch games with react query

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -1,8 +1,13 @@
 import axios from "axios";
 
+export interface FetchResponse<T> {
+  count: number;
+  results: T[];
+}
+
 export default axios.create({
-  baseURL: 'https://api.rawg.io/api',
+  baseURL: "https://api.rawg.io/api",
   params: {
-    key: '0d51710f66534efdb182e59418de7211'
-  }
-})
+    key: "0d51710f66534efdb182e59418de7211",
+  },
+});
